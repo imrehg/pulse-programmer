92878495d952350e4679303c3b3158b6bd186ade
ppham <ppham@fd086e45-922e-0410-a8fe-c19c5c766b4a>
2009-07-16 06:00:03 +0000

    Added comments, fixed write_data to be a string of hex literals.
    git-svn-id: https://pulse-sequencer.svn.sourceforge.net/svnroot/pulse-sequencer@419 fd086e45-922e-0410-a8fe-c19c5c766b4a

--- software/python/test_i2c.py
+++ software/python/test_i2c.py
@@ -1,8 +1,20 @@
 from test_config import *
 
+parse_params(('dummy', 'int'))
+
+# Slave addresses are the following:
+# 0x60 sequencer-side LED I2C driver
+# 0x61 breakout-side LED I2C driver
+#   (this will freeze up PTP firmware if breakout board is not connected)
+ 
 setup()
-send_i2c(slave_address = 0x61,
-         write_data = '\xFF')
+send_i2c(slave_address = 0x60,
+         write_data = '\x44\xFF')
+
+# write_data consists of two bytes
+# First byte: 0x44 means to write the following data and update output
+# Second byte: 0xFF means all high (turn on all LEDs), etc.
+
 teardown()
 
 
