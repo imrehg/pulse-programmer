96a67daf8ad0cce6e0cd98d5d194a0d3b29e60bc
solowanderery2k <solowanderery2k@fd086e45-922e-0410-a8fe-c19c5c766b4a>
2010-01-13 02:25:25 +0000

    Added script to configure ADF4360-2 eval board.
    git-svn-id: https://pulse-sequencer.svn.sourceforge.net/svnroot/pulse-sequencer@434 fd086e45-922e-0410-a8fe-c19c5c766b4a

---
+++ software/python/set_clk.py
@@ -0,0 +1,88 @@
+# Authored by Jeff Booth
+
+from test_config import *
+
+parse_params(('dummy', 'int'))
+
+# Slave addresses are the following:
+# 0x60 sequencer-side LED I2C driver
+# 0x61 breakout-side LED I2C driver
+#   (this will freeze up PTP firmware if breakout board is not connected)
+ 
+setup()
+#send_i2c(slave_address = 0x60,
+#         write_data = '\x44\x00')
+
+# write_data consists of two bytes
+# First byte: 0x44 means to write the following data and update output
+# Second byte: 0xFF means all high (turn on all LEDs), etc.
+
+# To initialize the clock source, we need to set up the three
+# registers on the board chip.
+
+# We send the R register first, followed by the control
+# register, and lastly the N register.
+CONTROL_REG = 1
+N_REG = 2
+R_REG = 0
+
+# Stores the data to transmit
+data = [0,0,0]
+
+# Control register
+data[CONTROL_REG] = int('100011111111000100001100',2)
+#data[CONTROL_REG] = int('100011111111000100001000',2)
+
+# N register
+data[N_REG] = int('000000001111101000000010',2)
+#data[N_REG] = int('000000011000011001010010',2)
+
+# R register
+data[R_REG] = int('000000000000000011001001',2)
+#data[R_REG] = int('000000000000000011001001',2)
+
+# Register size, in bits
+REGISTER_BITS = 24
+
+# I2C slave address
+I2C_SLAVE = 0x61
+
+# Command to write
+I2C_COMMAND = "\x44%c"
+
+# These are masks that indicate which pin
+# corresponds to which data bit.  We'll
+# OR them together to turn on multiple bits.
+LE_PIN = 1 << 5
+CLOCK_PIN = 1 << 7
+DATA_PIN = 1 << 6
+# TODO: check these pin mappings!
+
+# To make sure setup and hold time are met,
+# we'll first set the data pins, then raise the
+# clock pin, then lower the clock pin.  This
+# means each bit will require three commands.
+
+BIT_MASK = 1 << (REGISTER_BITS - 1)
+for entry in data:
+	print entry
+	# Transmit bits
+	for i in xrange(REGISTER_BITS):
+		bit_mask = (255^DATA_PIN if (entry & BIT_MASK) else 255)
+		print list(I2C_COMMAND % bit_mask)
+
+		send_i2c(I2C_SLAVE, I2C_COMMAND % bit_mask)
+		send_i2c(I2C_SLAVE, I2C_COMMAND % (bit_mask^CLOCK_PIN))
+		#send_i2c(I2C_SLAVE, I2C_COMMAND % bit_mask)
+		entry <<= 1
+
+	# Set the LE register high to store data in the
+	# appropriate register.
+	send_i2c(I2C_SLAVE, I2C_COMMAND % 255)
+	send_i2c(I2C_SLAVE, I2C_COMMAND % (255^LE_PIN))
+
+send_i2c(I2C_SLAVE, "\x44\xFF")
+
+teardown()
+
+
