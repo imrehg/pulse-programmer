313d35b3b694f8a7959f7239c691c49474b8de88
rmcclur <rmcclur@fd086e45-922e-0410-a8fe-c19c5c766b4a>
2010-04-23 01:01:39 +0000

    Fixed test_write issues - changed list arg to str
    git-svn-id: https://pulse-sequencer.svn.sourceforge.net/svnroot/pulse-sequencer@460 fd086e45-922e-0410-a8fe-c19c5c766b4a

--- software/python/test_write.py
+++ software/python/test_write.py
@@ -5,7 +5,6 @@ parse_params(('dummy', 'int'))
 print("Dummy option = " + str(sequencer.params.dummy))
 
 setup()
-payload = write_imem(0x20, ['\x11', '\x22', '\x33', '\x44',
-                           '\x55', '\x66', '\x77', '\x88'])
+payload = write_imem(0x20, "\x11\x22\x33\x44\x55\x66\x77\x88")
 teardown()
 
